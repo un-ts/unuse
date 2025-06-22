49defb76b331603dd51d61c435716ba4cc2f6b57
Shinigami92 <chrissi92@hotmail.de>
2025-06-22 19:23:38 +0200

    test: configure coverage

---
+++ vitest.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      all: true,
+      provider: 'v8',
+      reporter: ['clover', 'cobertura', 'json-summary', 'json', 'lcov', 'text'],
+      exclude: ['examples', '**/*.config.ts'],
+      reportOnFailure: true,
+      thresholds: {
+        // TODO @Shinigami92 2025-06-22: should be:
+        // lines: 90,
+        // statements: 90,
+        // functions: 90,
+        // branches: 85,
+
+        // for now:
+        lines: 40,
+        statements: 40,
+        functions: 70,
+        branches: 70,
+      },
+    },
+    reporters: process.env.CI
+      ? ['default', 'github-actions']
+      : [['default', { summary: false }]],
+  },
+});
