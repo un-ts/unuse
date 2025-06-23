eb97701dd6910c40b0becb00adbcc88abaa06395
Shinigami <chrissi92@hotmail.de>
2025-06-23 06:52:34 +0200

    test: playwright (#21)

---
+++ examples/react/playwright.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const IS_CI = !!process.env.CI;
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: IS_CI,
+  retries: IS_CI ? 2 : 0,
+  workers: IS_CI ? 1 : undefined,
+  reporter: IS_CI
+    ? [['list'], ['github']]
+    : [['html', { open: 'never' }], ['list']],
+  use: {
+    baseURL: 'http://localhost:6001',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'pnpm run dev --port 6001',
+    url: 'http://localhost:6001',
+    reuseExistingServer: !IS_CI,
+  },
+});
