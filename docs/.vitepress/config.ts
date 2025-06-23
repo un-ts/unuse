3b1d01bea91b4bd2db68632a5d64f1169b63df4c
Shinigami <chrissi92@hotmail.de>
2025-06-23 06:56:59 +0200

    docs: setup vitepress (#25)

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+  title: 'unuse',
+  description: 'Documentation Website for unuse',
+
+  base: process.env.NODE_ENV === 'production' ? '/unuse/' : '/',
+
+  themeConfig: {
+    nav: [{ text: 'Home', link: '/' }],
+
+    editLink: {
+      pattern: 'https://github.com/un-ts/unuse/edit/main/docs/:path',
+      text: 'Suggest changes to this page',
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/un-ts/unuse' },
+      { icon: 'npm', link: 'https://www.npmjs.com/package/unuse' },
+    ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2025-present unuse.',
+    },
+  },
+});
