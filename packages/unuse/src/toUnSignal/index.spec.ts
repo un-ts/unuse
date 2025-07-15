cb3d06d44aa3f5d32d177b93e8145cd0ca39686b
Shinigami <chrissi92@hotmail.de>
2025-07-15 17:06:26 +0200

    test: toUnSignal (#77)

--- packages/unuse/src/toUnSignal/index.spec.ts
+++ packages/unuse/src/toUnSignal/index.spec.ts
@@ -1,6 +1,6 @@
-import { unSignal } from 'unuse-reactivity';
-import { describe, expect, it } from 'vitest';
-import { toUnSignal } from '.';
+import { isUnSignal, unSignal } from 'unuse-reactivity';
+import { describe, expect, it, vi } from 'vitest';
+import { overrideToUnSignalFn, toUnSignal } from '.';
 
 describe('toUnSignal', () => {
   it('should be defined', () => {
@@ -12,6 +12,19 @@ describe('toUnSignal', () => {
 
     const actual = toUnSignal(state);
     expect(actual).toBe(state);
+  });
+
+  it('should return an empty UnSignal', () => {
+    const value = undefined;
+    const actual = toUnSignal(value);
+    expect(actual).toSatisfy(isUnSignal);
+    // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+    expect(actual.get()).toBeUndefined();
+  });
+
+  it('should return a UnSignal with null', () => {
+    const actual = toUnSignal(null);
+    expect(actual.get()).toBeNull();
   });
 
   it('should maintain the reactivity of the signal', () => {
@@ -27,5 +40,15 @@ describe('toUnSignal', () => {
 
     state.set(200);
     expect(actual.get()).toBe(200);
+  });
+
+  it('should be hookable for custom frameworks', () => {
+    const fnSpy = vi.fn();
+
+    overrideToUnSignalFn(fnSpy);
+    expect(fnSpy).not.toHaveBeenCalled();
+
+    toUnSignal('test');
+    expect(fnSpy).toHaveBeenCalledWith('test');
   });
 });
